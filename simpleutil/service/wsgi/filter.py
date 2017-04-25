2c72a6e251747c0f8f1ea120d4c598207634b426
lolizeppelin <lolizeppelin@gmail.com>
2017-04-25 15:11:10 +0800

    增加过滤器

--- simpleutil/service/wsgi/filter.py
+++ simpleutil/service/wsgi/filter.py
@@ -8,11 +8,30 @@ import webob.exc
 class FilterBase(object):
     NAME = 'FilterBase'
 
+    @classmethod
+    def factory(cls, global_conf, **local_conf):
+        """Factory method for paste.deploy.
+
+        :param global_conf: dict of options for all middlewares
+                            (usually the [DEFAULT] section of the paste deploy
+                            configuration file)
+        :param local_conf: options dedicated to this middleware
+                           (usually the option defined in the middleware
+                           section of the paste deploy configuration file)
+        """
+        conf = global_conf.copy() if global_conf else {}
+        conf.update(local_conf)
+
+        def middleware_filter(app):
+            return cls(app)
+
+        return middleware_filter
+
     def __init__(self, application):
         self.application = application
 
     @abc.abstractmethod
-    def process_request(self, request):
+    def process_request(self, req):
         """Called on each request.
         If this returns None, the next application down the stack will be
         executed. If it returns a response then that response will be returned
@@ -23,15 +42,14 @@ class FilterBase(object):
         webob将通过req.Response类生成Response实例再调用__call__方法
         返回值不是None表明过滤失败
         """
-
-    def process_response(self, request, response):
+    def process_response(self, req, response):
         """Do whatever you'd like to the response, based on the request."""
         return response
 
     @webob.dec.wsgify()
-    def __call__(self, request):
-        response = self.process_request(request)
+    def __call__(self, req):
+        response = self.process_request(req)
         if response:
             return response
-        response = request.get_response(self.application)
-        return self.process_response(request, response)
+        response = req.get_response(self.application)
+        return self.process_response(req, response)
