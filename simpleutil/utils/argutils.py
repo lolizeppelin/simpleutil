bb6b3f719c5200694f79845e2cfe87932d153454
lolizeppelin <lolizeppelin@gmail.com>
2017-05-22 18:09:14 +0800

    增加一个参数用于格式化id

--- simpleutil/utils/argutils.py
+++ simpleutil/utils/argutils.py
@@ -16,13 +16,16 @@ class Idformater(object):
     This class will format server_id on kwargs
     Instance must has attr all_id
     这里的描述器实现参考来源是webob.dec.wsgify
-    作用在于格式化传入的id转为int组成的list
+    作用在于格式化传入的id转为校验后的list
     使用这个描述其的类必须有一个all_id属性返回为set
+    all_key不为None的时候,当id值为all_key的时候表示所有id
+    formatfunc每个id都被formatfunc格式化处理
     """
-    def __init__(self, func=None, key='id', all_key="all"):
+    def __init__(self, func=None, key='id', all_key=None, formatfunc=None):
         self.func = func
         self.key = key
         self.all_key = all_key
+        self.formatfunc=formatfunc
         self.all_id = None
 
     def __get__(self, instance, owner):
@@ -31,7 +34,8 @@ class Idformater(object):
         if hasattr(self.func, '__get__'):
             formater = self.__class__(func=self.func.__get__(instance, owner),
                                       key=self.key,
-                                      zero_as_all=self.all_key)
+                                      all_key=self.all_key,
+                                      formatfunc=self.formatfunc)
             formater.all_id = instance.all_id
             return formater
         else:
@@ -41,20 +45,15 @@ class Idformater(object):
         if self.func is None:
             return self.__class__(func=args[0],
                                   key=self.key,
-                                  all_key=self.all_key)
+                                  all_key=self.all_key,
+                                  formatfunc=self.formatfunc)
         else:
             id_string = kwargs.pop(self.key, None)
             if not isinstance(id_string, basestring):
                 raise InvalidArgument('%s not basestring' % self.key)
-            if id_string.isdigit():
-                id_set = set()
-                id_set.add(int(id_string))
-            else:
-                id_set = set(id_string.split(','))
-                try:
-                    id_set = set(map(int, id_set))
-                except TypeError:
-                    pass
+            id_set = set(id_string.split(','))
+            if self.formatfunc:
+                id_set = set(map(self.formatfunc, id_set))
             if self.all_key is not None:
                 if self.all_key in id_set and len(id_set) == 1:
                     id_set = self.all_id
