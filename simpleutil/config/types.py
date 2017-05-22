5996aca91b4474bc9fdbee42386f67b3ca2572ba
lolizeppelin <lolizeppelin@gmail.com>
2017-05-22 18:08:48 +0800

    增加可以多次定义的模块字符串配置类型

--- simpleutil/config/types.py
+++ simpleutil/config/types.py
@@ -702,3 +702,31 @@ class ImportString(ConfigType):
 
     def _formatter(self, value):
         return value
+
+
+class MultiImportString(ImportString):
+
+    def __init__(self, type_name='multi import string'):
+        super(ImportString, self).__init__(type_name=type_name)
+
+
+    def __call__(self, value):
+        if isinstance(value, list):
+            return self._formatter(value)
+        else:
+            return ImportString.__call__(self, value)
+
+    def format_defaults(self, default, sample_default=None):
+        """Return a list of formatted default values.
+
+        """
+        if sample_default is not None:
+            default_list = self._formatter(sample_default)
+        elif not default:
+            default_list = []
+        else:
+            default_list = self._formatter(default)
+        return default_list
+
+    def _formatter(self, value):
+        return [self.quote_trailing_and_leading_space(v) for v in value]
