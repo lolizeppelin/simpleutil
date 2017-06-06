f9b826075fdd7063a031c26326cfb957dab0c013
lolizeppelin <lolizeppelin@gmail.com>
2017-06-06 11:20:55 +0800

    异常继承修改

--- simpleutil/common/exceptions.py
+++ simpleutil/common/exceptions.py
@@ -3,7 +3,7 @@ class InvalidInput(Exception):
     def __init__(self, msg):
         self.message = InvalidInput.message + msg
 
-class InvalidArgument(InvalidInput):
+class InvalidArgument(Exception):
     message = "Invalid argument: "
     def __init__(self, msg):
         self.message = InvalidArgument.message + msg
