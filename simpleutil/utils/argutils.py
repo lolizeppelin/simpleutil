cec336d467a9f28a3b302cd4e0fb9a767d5b1b70
lolizeppelin <lolizeppelin@gmail.com>
2017-06-07 18:03:48 +0800

    稍微改进一下支持单key

--- simpleutil/utils/argutils.py
+++ simpleutil/utils/argutils.py
@@ -21,13 +21,16 @@ class Idformater(object):
     这里的描述器实现参考来源是webob.dec.wsgify
     作用在于格式化传入的id转为校验后的list
     使用这个描述其的类必须继承自IdformaterBase
-    all_key不为None的时候,当id等于all_key的时候表示所有id
+    magic == 'all' 意味着all能表示所有key
+    magic == 'onlyone' 标志只允许一个key
     formatfunc每个id都被formatfunc格式化处理
     """
-    def __init__(self, func=None, key='id', all_key=None, formatfunc=None):
+    def __init__(self, func=None, key='id', magic="onlyone", formatfunc=None):
         self.func = func
         self.key = key
-        self.all_key = all_key
+        if magic not in ('all', 'onlyone'):
+            raise ValueError("Magic not in 'onlyone' or 'all'")
+        self.magic = magic
         self.formatfunc = formatfunc
         self.all_id = None
 
@@ -37,9 +40,9 @@ class Idformater(object):
         if hasattr(self.func, '__get__'):
             formater = self.__class__(func=self.func.__get__(instance, owner),
                                       key=self.key,
-                                      all_key=self.all_key,
+                                      magic=self.magic,
                                       formatfunc=self.formatfunc)
-            if self.all_key is not None:
+            if self.magic == 'all':
                 formater.all_id = instance.all_id
             return formater
         else:
@@ -49,7 +52,7 @@ class Idformater(object):
         if self.func is None:
             return self.__class__(func=args[0],
                                   key=self.key,
-                                  all_key=self.all_key,
+                                  magic=self.magic,
                                   formatfunc=self.formatfunc)
         else:
             id_string = kwargs.pop(self.key, None)
@@ -57,11 +60,11 @@ class Idformater(object):
                 raise InvalidArgument('%s is None or not basestring' % self.key)
             match_all = False
             id_set = set(id_string.split(','))
-            if self.all_key is not None:
-                if self.all_key in id_set and len(id_set) == 1:
+            if self.magic == "all":
+                if self.magic in id_set and len(id_set) == 1:
                     id_set = self.all_id
                     match_all = True
-                elif self.all_key in id_set and len(id_set) > 1:
+                elif self.magic in id_set and len(id_set) > 1:
                     raise InvalidArgument('%s:0 with other id in same list' % self.key)
             if self.formatfunc and not match_all:
                 try:
@@ -73,5 +76,7 @@ class Idformater(object):
                                            })
             if len(id_set) < 1:
                 raise InvalidArgument('No entity found for %s' % self.key)
-            kwargs[self.key] = id_set
+            if self.magic == 'onlyone' and len(id_set) > 1:
+                raise InvalidArgument('More then one entity found')
+            kwargs[self.key] = id_set.pop() if self.magic == 'onlyone' else id_set
         return self.func(*args, **kwargs)
