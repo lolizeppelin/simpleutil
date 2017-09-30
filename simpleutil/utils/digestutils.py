6247142ae35d7bc87b459a1fca7e3cd688540919
lolizeppelin <lolizeppelin@gmail.com>
2017-09-30 17:54:14 +0800

    使用上下文方式

--- simpleutil/utils/digestutils.py
+++ simpleutil/utils/digestutils.py
@@ -1,42 +1,37 @@
 import os
 import zlib
 import hashlib
-
+import contextlib
 import six
 
 from simpleutil.common import exceptions
 
-
-def openfile(file_path):
-    if not os.path.exists(file_path) or not os.access(file_path, os.R_OK):
+@contextlib.contextmanager
+def openfile(path):
+    if not os.path.exists(path) or not os.access(path, os.R_OK):
         raise exceptions.InvalidArgument('file not exist or can not be read')
+    f = open(path, 'rb')
     try:
-        f = open(file_path, 'rb')
-    except IOError:
-        raise exceptions.InvalidArgument('open file io error')
-    except OSError:
-        raise exceptions.InvalidArgument('open file os error')
-    return f
+        yield f
+    finally:
+        f.close()
 
 
-def filemd5(file_path):
+def filemd5(path):
     md5_instance = hashlib.md5()
-    f = openfile(file_path)
-    bytes = f.read(10240)
-    while(bytes != six.binary_type('')):
-        md5_instance.update(bytes)
+    with openfile(path) as f:
         bytes = f.read(10240)
-    f.close()
-    md5value = md5_instance.hexdigest()
-    return md5value
+        while(bytes != six.binary_type('')):
+            md5_instance.update(bytes)
+            bytes = f.read(10240)
+        return md5_instance.hexdigest()
 
 
-def filecrc32(file_path):
-    f = openfile(file_path)
-    bytes = f.read(10240)
-    crc = 0
-    while(bytes != six.binary_type('')):
-        crc = zlib.crc32(bytes, crc)
+def filecrc32(path):
+    with openfile(path) as f:
         bytes = f.read(10240)
-    f.close()
-    return '%x' % (crc & 0xffffffff)
+        crc = 0
+        while(bytes != six.binary_type('')):
+            crc = zlib.crc32(bytes, crc)
+            bytes = f.read(10240)
+        return '%x' % (crc & 0xffffffff)
