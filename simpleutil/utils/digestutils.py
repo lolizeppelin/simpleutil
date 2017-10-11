397a23d8ddb69e9f624e43dffc745a73c636876f
lolizeppelin <lolizeppelin@gmail.com>
2017-10-11 17:15:00 +0800

    BLOCK SIZE 定义在外部

--- simpleutil/utils/digestutils.py
+++ simpleutil/utils/digestutils.py
@@ -6,6 +6,8 @@ import six
 
 from simpleutil.common import exceptions
 
+BLOCK = 4096
+
 @contextlib.contextmanager
 def openfile(path):
     if not os.path.exists(path) or not os.access(path, os.R_OK):
@@ -20,18 +22,18 @@ def openfile(path):
 def filemd5(path):
     md5_instance = hashlib.md5()
     with openfile(path) as f:
-        bytes = f.read(10240)
+        bytes = f.read(BLOCK)
         while(bytes != six.binary_type('')):
             md5_instance.update(bytes)
-            bytes = f.read(10240)
+            bytes = f.read(BLOCK)
         return md5_instance.hexdigest()
 
 
 def filecrc32(path):
     with openfile(path) as f:
-        bytes = f.read(10240)
+        bytes = f.read(BLOCK)
         crc = 0
         while(bytes != six.binary_type('')):
             crc = zlib.crc32(bytes, crc)
-            bytes = f.read(10240)
+            bytes = f.read(BLOCK)
         return '%x' % (crc & 0xffffffff)
