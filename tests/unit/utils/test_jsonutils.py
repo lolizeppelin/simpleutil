c4d111e8961c7d67ae2f84d96846355ad42aeac0
lolizeppelin <lolizeppelin@gmail.com>
2017-11-23 18:19:32 +0800

    测试内容增加

--- tests/unit/utils/test_jsonutils.py
+++ tests/unit/utils/test_jsonutils.py
@@ -1,5 +1,5 @@
+import json
 from simpleutil.utils import jsonutils
-
 
 buffer = None
 
@@ -7,3 +7,27 @@ try:
     print jsonutils.loads_as_bytes(buffer)
 except TypeError:
     print 'test success'
+
+
+
+
+n = {'a': [u"8000-9000", u"9500-10000"], 'b':2}
+u = jsonutils.dumps(n)
+
+
+n = [{'a':1, 'b': 2}, {'a':1, 'b': 2},]
+n = ['a', 'b', {'a': 1}]
+u = jsonutils.dumps(n)
+
+
+print u
+
+# print u, type(u)
+#
+#
+# v = json.dumps(default)
+# print v, type(u)
+#
+#
+
+print jsonutils.loads_as_bytes(u)
