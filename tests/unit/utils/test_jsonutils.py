7f763cf6bd7b2fe963fd8f5b4aec8ad900a688c6
lolizeppelin <lolizeppelin@gmail.com>
2017-11-25 21:48:35 +0800

    json校验相关更新

--- tests/unit/utils/test_jsonutils.py
+++ tests/unit/utils/test_jsonutils.py
@@ -1,33 +1,33 @@
 import json
 from simpleutil.utils import jsonutils
 
-buffer = None
 
-try:
-    print jsonutils.loads_as_bytes(buffer)
-except TypeError:
-    print 'test success'
+RESPONESCHEMA = {
+    'type': 'object',
+    'required': ['agent_id', 'agent_time', 'resultcode'],
+    'properties':
+        {
+            'agent_id': {'type': 'integer', 'minimum': 0},                                   # agent id
+            'agent_time': {'type': 'integer', 'minimum': 0},                                 # agent respone time
+            'resultcode': {'type': 'integer', 'minimum': -127, 'maxmum': 127},               # resultcode
+            'result': {'type': 'string'},                                                    # result message
+            'details': {'type': 'array', 'minItems': 1,                                      # details for rpc
+                        'items': {'type': 'object',
+                                  'required': ['detail_id', 'resultcode', 'result'],
+                                  'properties': {
+                                      'detail_id': {'type': 'integer', 'minimum': 0},
+                                      'resultcode': {'type': 'integer', 'minimum': -127, 'maxmum': 127},
+                                      'result': {'anyOf': [{'type': 'string'}, {'type': 'object'}]}}
+                                  }
+                        }
+        }
+}
 
 
+x = {'agent_time': 1511616056, 'expire': 60, 'result': 'Get status from 127.0.0.1 success',
+     'details': [{'resultcode': 0, 'detail_id': 0,
+                  'result': {'frozen': False, 'endpoint': 'gopcdn', 'locked': 0, 'entitys': 0}}],
+     'resultcode': 0, 'agent_id': 1}
 
 
-n = {'a': [u"8000-9000", u"9500-10000"], 'b':2}
-u = jsonutils.dumps(n)
-
-
-n = [{'a':1, 'b': 2}, {'a':1, 'b': 2},]
-n = ['a', 'b', {'a': 1}]
-u = jsonutils.dumps(n)
-
-
-print u
-
-# print u, type(u)
-#
-#
-# v = json.dumps(default)
-# print v, type(u)
-#
-#
-
-print jsonutils.loads_as_bytes(u)
+print jsonutils.schema_validate(x, RESPONESCHEMA)
